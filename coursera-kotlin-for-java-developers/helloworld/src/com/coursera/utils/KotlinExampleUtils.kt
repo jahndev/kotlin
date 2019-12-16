1e58a10d20c0ccf88bd70f3f7b414c7b2bfdf8e0
jahndev <jahnarellano85.dev@gmail.com>
2019-12-16 01:55:56 -0300

    Checking identifier exercise

---
+++ coursera-kotlin-for-java-developers/helloworld/src/com/coursera/utils/KotlinExampleUtils.kt
@@ -0,0 +1,26 @@
+package com.coursera.utils
+
+fun exampleLoop() : Unit  {
+    for (c in '0' until '9') {
+        print(c)
+    }
+}
+
+fun exampleLoop2() = listOf(1 in 1..9)
+
+fun isValidIdentifier(s: String): Boolean {
+    var isValid : Boolean = true
+    if(s.isEmpty()) return false
+    s.forEachIndexed { index, c ->
+        isValid = isValid && when (c) {
+            ' ' -> index == 0
+            '_' -> index == 0
+            in 'a'..'z' -> true
+            in 'A'..'Z' -> true
+            in '0'..'9' -> index > 0
+            else -> false
+        }
+    }
+    return isValid
+}
+
--- coursera-kotlin-for-java-developers/helloworld/src/intro/helloworld.kt
+++ coursera-kotlin-for-java-developers/helloworld/src/intro/helloworld.kt
@@ -1,6 +1,25 @@
 package intro
 
+import com.coursera.utils.exampleLoop
+import com.coursera.utils.exampleLoop2
+import com.coursera.utils.isValidIdentifier
+
 fun main () {
     val name = "kotloin"
     println("Hello, $name!")
+    exampleLoop()
+
+    print("\n")
+    print("loop2 start\n")
+    exampleLoop2()
+    print("loop2 end\n")
+
+    //Checking identifier
+    println(isValidIdentifier("name"))   // true
+    println(isValidIdentifier("_name"))  // true
+    println(isValidIdentifier("_12"))    // true
+    println(isValidIdentifier(""))       // false
+    println(isValidIdentifier("012"))    // false
+    println(isValidIdentifier("no$"))    // false
+
 }
